b6ce0ac74e0f6f1cdb2fe9750d778fd3faed3ef9
viljaah <you@example.com>
2025-04-10 16:30:26 +0200

    merging

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,7 +6,6 @@ import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes';
 import Navbar from './components/shared/navbar/Navbar';
 
 // Pages
-import ArtifactApp from './pages/createStudy/createStudy';
 import LoginPage from './pages/auth/LoginPage'
 import SignupPage from './pages/auth/SignupPage'
 import DashPage from './pages/dashboard/DashPage';
@@ -17,20 +16,17 @@ import ParticipantPage from './pages/participant/ParticipantPage'
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        {/* <Route path="/edit/:studyId" element={<EditPage />}/> */}
-        <Route path="/results/:studyId" element={<ResultsPage />} />
-        <Route path="/createStudy" element={<CreateStudy />} />
-        <Route path="/participant/:studyId" element={<ParticipantPage />} />
-
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          {/* <Route path="/edit/:studyId" element={<EditPage />}/> */}
+          <Route path="/participant/:studyId" element={<ParticipantPage />} />
           <Route path="/dashboard" element={<ProtectedRoutes> <DashPage /> </ProtectedRoutes>}/>
           <Route path="/results/:studyId" element={<ProtectedRoutes> <ResultsPage /> </ProtectedRoutes>} />
-          <Route path="/createStudy" element={<ProtectedRoutes> <ArtifactApp /> </ProtectedRoutes>} />
-      </Routes>
-      </BrowserRouter>
+          <Route path="/createStudy" element={<ProtectedRoutes> <CreateStudy /> </ProtectedRoutes>} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
